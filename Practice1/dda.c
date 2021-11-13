87c385b3165fefb0f2a19fa88000b1e07dedf740
JaniceGar <yanisamper0123@gmail.com>
2021-11-12 20:06:03 -0600

    Create dda.c

---
+++ Practice1/dda.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <conio.h>
+#include <math.h>
+
+void main (){
+	int gdriver = DEFECT, gmode;
+	int x1, y1, x2, u, step, xn. yn, dx, dy;
+	clrscr();
+	initgraph()&gdriver, &gmode, "C:"
+}
+
