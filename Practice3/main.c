960a52f7505855786bf00a8c9c441915cafec97a
JaniceGar <yanisamper0123@gmail.com>
2021-12-01 19:38:40 -0600

    creating the matrix

--- Practice3/main.c
+++ Practice3/main.c
@@ -1,1 +1,20 @@
-//
+//transformation matrix
+
+
+
+/* Matriz bidimensional. */
+
+	#include <stdio.h>
+
+	int main() /* Rellenamos una matriz */
+	{
+		int x,i,numeros[3][4];
+		/* rellenamos la matriz */
+		for (x=0;x<3;x++)
+			for (i=0;i<4;i++)
+				scanf("%d",&numeros[x][i]);
+		/* visualizamos la matriz */
+		for (x=0;x<3;x++)
+			for (i=0;i<4;i++)
+				printf("%d",numeros[x][i]);
+	}
