6a6afddb816e9df06c062f789d53668dcad9ee4d
JaniceGar <yanisamper0123@gmail.com>
2021-12-03 20:18:06 -0600

    ready
    kirby

--- Practice4/main.cpp
+++ Practice4/main.cpp
@@ -9,26 +9,25 @@ using namespace std;
 
 void animacion (void){
 	readimagefile("0-0000.jpg", 40,40,10,120);
-	sleep(50);
+	Sleep(50);
 	readimagefile("1-0000.jpg", 40,40,10,120);
-	sleep(50);
+	Sleep(50);
 	readimagefile("2-0000.jpg", 40,40,10,120);
-	sleep(50);
+	Sleep(50);
 	readimagefile("3-0000.jpg", 40,40,10,120);
-	sleep(50);
+	Sleep(50);
 	readimagefile("4-0000.jpg", 40,40,10,120);
-	sleep(50);
+	Sleep(50);
 	readimagefile("5-0000.jpg", 40,40,10,120);
-	sleep(50);
+	Sleep(50);
 	readimagefile("6-0000.jpg", 40,40,10,120);
-	sleep(50);
+	Sleep(50);
 }
 
 
 int main (int argc, char *argv[]){
 	initwindow(400,400,"Kirby",400,250);
-	setcolor(15); 
-	cleardevice():
+	setcolor(15);  	cleardevice();
 		while (!kbhit()){
 			animacion();
 		}
