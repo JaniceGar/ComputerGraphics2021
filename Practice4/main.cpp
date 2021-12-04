275d1ddf1ced15ac1e1dab14a513badd652c3021
JaniceGar <yanisamper0123@gmail.com>
2021-12-03 18:06:41 -0600

    trying to create an animation

---
+++ Practice4/main.cpp
@@ -0,0 +1,25 @@
+#include <cstdlib>
+#include <iostream>
+#include <graphics.h>
+#include <windows.h>
+using namespace std;
+
+void animacion (void){
+	readimagefile("0-0000.jpg", 40,40,10,120);
+	sleep(50);
+	readimagefile("1-0000.jpg", 40,40,10,120);
+	sleep(50);
+	readimagefile("2-0000.jpg", 40,40,10,120);
+	sleep(50);
+	readimagefile("3-0000.jpg", 40,40,10,120);
+	sleep(50);
+	readimagefile("4-0000.jpg", 40,40,10,120);
+	sleep(50);
+	readimagefile("5-0000.jpg", 40,40,10,120);
+	sleep(50);
+	readimagefile("6-0000.jpg", 40,40,10,120);
+	sleep(50);
+}
+
+
+
